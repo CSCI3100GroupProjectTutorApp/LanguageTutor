d366a6fa1409849974eae5fdc39669133a366b29
hooh777 <hoohhooh777@gmail.com>
2025-04-08 01:18:15 +0800

    adjust config.py for test_auth

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,10 +3,12 @@ from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()
+load_dotenv("test/test_db.env")  # Load test_db.env for testing
 
 class Settings(BaseSettings):
     # MongoDB
     MONGODB_URL: str = os.getenv("MONGODB_URL")
+    MONGODB_DB_NAME: str = "language_tutoring_app"
     
     # JWT
     SECRET_KEY: str = os.getenv("SECRET_KEY")
