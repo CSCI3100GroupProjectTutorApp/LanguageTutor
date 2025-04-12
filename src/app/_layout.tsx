d5f2d768579eb190c9293924c79740caa7260580
lipinglam <164549878+lpl1001@users.noreply.github.com>
2025-04-12 18:13:29 +0800

    Add new stack for translate page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,7 @@ import {Stack} from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Platform} from "react-native";
 
 
 export default function RootLayout(){
@@ -11,7 +12,8 @@ export default function RootLayout(){
             <Stack>
                 <Stack.Screen name ='(auths)' options={{headerShown: false}}/>
                 <Stack.Screen name ='(tabs)' options={{headerShown: false}}/>
-                <Stack.Screen name ='word' options={{headerShown: false}}/>
+                <Stack.Screen name ='translate' options={{headerShown: false}}/>
+                <Stack.Screen name ='word' options={{headerShown: false}}/>          
             </Stack>
             <StatusBar style="dark" backgroundColor="transparent"/>
             </SafeAreaProvider>
@@ -19,3 +21,6 @@ export default function RootLayout(){
         
     );
 }
+const styles = StyleSheet.create({
+
+})
