8cea83f972bed12f9550131971345a2f436e6904
lipinglam <164549878+lpl1001@users.noreply.github.com>
2025-05-09 23:34:03 +0800

    Update extra stacks

--- src/app/settings/_layout.tsx
+++ src/app/settings/_layout.tsx
@@ -8,10 +8,6 @@ const settingsLayout = () => {
             options={{headerShown:false}}/>
             <Stack.Screen name='license' 
             options={{headerShown:false}}/>
-            <Stack.Screen name='FQA' 
-            options={{headerShown:false}}/>
-            <Stack.Screen name='about' 
-            options={{headerShown:false}}/>
         </Stack>
   )
 }
